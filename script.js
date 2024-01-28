6c1c2432f932aa0805950383a412a0ce9ed6feef
Bilal Tariq <hafizbilaltariq360@gmail.com>
2024-01-28 18:14:33 +0500

    Update

--- script.js
+++ script.js
@@ -42,20 +42,3 @@ let nav = document.querySelector("header nav");
 mobileMenuIcon.addEventListener("click", () => {
   nav.classList.toggle("show");
 });
-
-window.onscroll = () => {
-  sections.forEach((sec) => {
-    let top = window.scrollY;
-    let offset = sec.offsetTop - 150;
-    let height = sec.offsetHeight;
-    let id = sec.getAttribute("id");
-    if (top >= offset && top < offset + height) {
-      navLinks.forEach((links) => {
-        links.classList.remove("active");
-        document
-          .querySelector("header nav a[href*=" + id + "]")
-          .classList.add("active");
-      });
-    }
-  });
-};
